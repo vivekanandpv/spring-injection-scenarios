8492d8b9eaeffee3b96a5df2efd5036410c06b17
vivek <vivek@athenaeum.in>
2021-05-27 11:28:25 +0530

    done

--- src/main/java/com/example/springinjectionscenarios/controller/SampleController.java
+++ src/main/java/com/example/springinjectionscenarios/controller/SampleController.java
@@ -9,16 +9,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1/sample")
 public class SampleController {
-    //  Dependency as a field, but exposes a setter
-    private VehicleService vehicleService;
+    //  field is final, so that it can only be assigned within constructor or in place
+    private final VehicleService vehicleService;
 
-    //  Setter injection
-    //  Still, the developer has to remember to set the field
-    //  But the class itself is more transparent
-    //  There is still a risk of initializing the class without proper
-    //  dependencies, particularly in unit testing
-    @Autowired  //  autowired is required as of this version
-    public void setVehicleService(VehicleService vehicleService) {
+    //  This class will not instantiate without proper dependencies provided
+
+    //  It is an error to declare multiple constructors while we are using
+    //  automated DI system such as Spring. Spring may pick the overload that has
+    //  least number of arguments. This may put the application is unpredictable
+    //  condition. So, the recommendation is to have only one constructor that initializes
+    //  all the dependencies. Also, make the dependencies final.
+    //  Multiple dependencies can be declared in any order in the constructor.
+
+    //  Here @Autowired is not required, but does no harm if present
+    public SampleController(VehicleService vehicleService) {
         this.vehicleService = vehicleService;
     }
 
