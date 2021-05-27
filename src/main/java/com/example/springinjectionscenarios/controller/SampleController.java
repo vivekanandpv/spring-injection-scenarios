3bf27ff8f89809b6aea74ad0a64aa48cb77e8b30
vivek <vivek@athenaeum.in>
2021-05-27 11:09:51 +0530

    done

--- src/main/java/com/example/springinjectionscenarios/controller/SampleController.java
+++ src/main/java/com/example/springinjectionscenarios/controller/SampleController.java
@@ -1,5 +1,7 @@
 package com.example.springinjectionscenarios.controller;
 
+import com.example.springinjectionscenarios.component.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1/sample")
 public class SampleController {
+    //  Don't use this
+    //  The dependencies this way are opaque
+    //  Thus, this class is not testable
+    @Autowired
+    private VehicleService vehicleService;
 
     @GetMapping
     public String get() {
-        return "Hi there!";
+        return vehicleService.getMessage();
     }
 }
