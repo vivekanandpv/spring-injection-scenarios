422c758e55f8a786a1b9e489589dd6380850bb85
vivek <vivek@athenaeum.in>
2021-05-27 11:37:01 +0530

    done

---
+++ src/main/java/com/example/springinjectionscenarios/component/IVehicleService.java
@@ -0,0 +1,5 @@
+package com.example.springinjectionscenarios.component;
+
+public interface IVehicleService {
+    String getMessage();
+}
--- src/main/java/com/example/springinjectionscenarios/component/VehicleService.java
+++ src/main/java/com/example/springinjectionscenarios/component/VehicleService.java
@@ -5,14 +5,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
-
-//  also possible: @Configuration, @Repository
-//  Because, all these are applied over @Component
-//  It is recommended to use them for their specialized purpose
-//  because, the concerns they deal with are different and in future
-//  they may get additional functionalities
-@Service
-public class VehicleService {
+@Component
+public class VehicleService implements IVehicleService {
     public String getMessage() {
         return "Vehicle Service";
     }
