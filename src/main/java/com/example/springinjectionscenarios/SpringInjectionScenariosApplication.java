6f1cfe09003d95eb40a7e269f590af2aa531cf4d
vivek <vivek@athenaeum.in>
2021-05-27 11:01:58 +0530

    Initial commit

---
+++ src/main/java/com/example/springinjectionscenarios/SpringInjectionScenariosApplication.java
@@ -0,0 +1,13 @@
+package com.example.springinjectionscenarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInjectionScenariosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringInjectionScenariosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springinjectionscenarios/SpringInjectionScenariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springinjectionscenarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInjectionScenariosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
