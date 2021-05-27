aec5e3118cedc8fb772ebf53e05e1534c2a5854a
vivek <vivek@athenaeum.in>
2021-05-27 11:33:07 +0530

    done

--- src/main/java/com/example/springinjectionscenarios/component/VehicleService.java
+++ src/main/java/com/example/springinjectionscenarios/component/VehicleService.java
@@ -1,8 +1,17 @@
 package com.example.springinjectionscenarios.component;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
-@Component
+
+//  also possible: @Configuration, @Repository
+//  Because, all these are applied over @Component
+//  It is recommended to use them for their specialized purpose
+//  because, the concerns they deal with are different and in future
+//  they may get additional functionalities
+@Service
 public class VehicleService {
     public String getMessage() {
         return "Vehicle Service";
